78a574e034721bd8ca8cc99fb9359ae818fe5c81
Daoud LAMALMI <102736413+MadokaIII@users.noreply.github.com>
2023-01-11 14:14:02 +0100

    minor changes to make sure Program is in ChessApp

---
+++ src/ChessApp/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ChessApp{
+
+    using Pieces;
+
+    class Program {
+
+        public static void Main(string[] args) {
+
+            int[] pos = { 3, 4 };
+            Pawn pawn = new Pawn(Color.BLACK, pos);
+            Console.WriteLine(pawn.PieceType);
+
+        }
+
+    }
+
+}
